11294beb06ac73d01242c99fff9f10bade34b1cc
joelaforet <joelaforetjr@yahoo.com>
2026-01-21 16:55:31 -0700

    set-up MDAnalysis exporter file

---
+++ mupt/interfaces/MDAnalysis/exporters.py
@@ -0,0 +1,11 @@
+"""
+MuPT to MDAnalysis Topology Exporter
+
+This module provides functionality to convert MuPT Representation objects
+(univprim) into MDAnalysis Universe objects, focusing on topology information
+(atoms, residues, segments, and bonds).
+"""
+
+__author__ = 'Joseph R. Laforet Jr.'
+__email__ = 'jola3134@colorado.edu'
+
