4b05dc98249863217165e3ef00b63a241412bf71
joelaforet <joelaforetjr@yahoo.com>
2026-01-21 16:56:14 -0700

    Added imports

--- mupt/interfaces/MDAnalysis/exporters.py
+++ mupt/interfaces/MDAnalysis/exporters.py
@@ -9,3 +9,12 @@ This module provides functionality to convert MuPT Representation objects
 __author__ = 'Joseph R. Laforet Jr.'
 __email__ = 'jola3134@colorado.edu'
 
+import MDAnalysis as mda
+from MDAnalysis.core.topologyattrs import Bonds
+
+import numpy as np
+from typing import Optional
+
+from ...mupr.primitives import Primitive
+from ...mutils.allatomutils import _is_AA_export_compliant
+from ...chemistry.core import BOND_ORDER
