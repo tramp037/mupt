0d710892d1fc3fcab234165dc70fcaf5cfcaa19f
joelaforet <joelaforetjr@yahoo.com>
2026-01-21 17:18:42 -0700

    Implemented a function that checks if a Primitive is SAAMR compliant

--- mupt/mutils/SAAMR.py
+++ mupt/mutils/SAAMR.py
@@ -4,4 +4,15 @@
 __author__ = 'Joseph R. Laforet Jr.'
 __email__ = 'jola3134@colorado.edu'
 
+from ..mupr.primitives import Primitive
 
+def _is_SAAMR_compliant(prim : Primitive) -> bool:
+    """
+    Check whether a Primitive hierarchy is organized
+    as universe -> molecule -> repeat-unit -> atom
+    """   
+    
+    return all(
+        leaf.is_atom and (leaf.depth == 3)        
+            for leaf in prim.leaves
+    )
